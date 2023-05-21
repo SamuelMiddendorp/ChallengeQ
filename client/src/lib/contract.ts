e435a1d78c6c708cc30b011dcdb9ce596179fc88
Samuel Middendorp <samiezaurus@gmail.com>
2023-05-21 10:31:35 +0200

    Add contracts to project and adjust store

---
+++ client/src/lib/contract.ts
@@ -0,0 +1,21 @@
+export interface QuestionRequest{
+    name: string,
+    description: string,
+    answers: Map<string, string>,
+}
+
+export interface Scoreboard{
+    players: ScoreboardEntry[]
+}
+
+export interface ScoreboardEntry{
+    username: string,
+    points: number
+}
+
+export interface AnswerResponse{
+    answer: string
+}
+export interface UserDetailsResponse{
+    username: string
+}
--- client/src/stores/PlayerWebSocketStore.ts
+++ client/src/stores/PlayerWebSocketStore.ts
@@ -1,28 +1,23 @@
 import { browser } from '$app/environment';
 import { writable, type Writable } from 'svelte/store';
+import type { QuestionRequest } from '../lib/contract';
 
-export interface Message{
-    userId: string,
-    message: string
-}
-export const messageStore: Writable<Message> = writable({userId: '', message: ''});
-export let sendMessage = (message: Message) => { };
+export const questionStore: Writable<QuestionRequest> = writable();
 
 // Connect locally only if store is in browser
-    const socket = browser ? new WebSocket('ws://localhost:3000') : null;
+const socket = browser ? new WebSocket('ws://localhost:3000') : null;
 
-    socket?.addEventListener('open', function (event) {
-        console.log("It's open");
-    });
+socket?.addEventListener('open', function (event) {
+    console.log("Connected to ChallengeQ");
+});
 
-    socket?.addEventListener('message', function (event) {
-        console.log("Message received")
-        console.log(event);
-        let data = JSON.parse(event.data);
-        messageStore.set({userId: data.id, message: data.message});
-    });
-    sendMessage = (message: Message) => {
-        if (socket!.readyState <= 1) {
-            socket?.send(JSON.stringify(message));
-        }
+socket?.addEventListener('message', function (event) {
+    let data: QuestionRequest = JSON.parse(event.data);
+    questionStore.set(data);
+});
+
+export const sendUserName = (username: string) => {
+    if (socket!.readyState <= 1) {
+        socket?.send(JSON.stringify({username: username}));
     }
+}
